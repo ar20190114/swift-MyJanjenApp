7c2f955ea1504946183da7b14610ab58113e6e7c
ar20190114 <skdr0724@gmail.com>
2023-01-14 15:47:21 +0900

    Initial Commit

---
+++ MyJankenApp/MyJankenAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyJankenAppApp.swift
+//  MyJankenApp
+//
+//  Created by ryotaban on 2023/01/14.
+//
+
+import SwiftUI
+
+@main
+struct MyJankenAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
